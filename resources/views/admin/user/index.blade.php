e2aff313b495bbaa63fb15e673ac70f938409365
Jeff Tan <jefftan1513@gmail.com>
2019-10-06 15:04:26 +0800

    Updated Management spelling

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -12,7 +12,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        User Mamangement
+        User Management
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-users"></i> Home</a></li>
