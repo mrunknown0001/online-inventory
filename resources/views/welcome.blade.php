c4a5e799d964e26222e9239971bf25ca24ea9c1b
Michael Adam <madamt0001@gmail.com>
2019-10-09 20:10:43 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,8 +30,95 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link rel='stylesheet' href="{{ asset('/packages/core/main.css') }}"  />
+    <link rel='stylesheet' href="{{ asset('/packages/daygrid/main.css') }}"  />
+    <script src="{{ asset('/packages/core/main.js') }} "></script>
+    <script src="{{ asset('/packages/interaction/main.js') }}"></script>
+    <script src="{{ asset('/packages/daygrid/main.js') }}"></script>
     
-    
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = yyyy + '-' + mm + '-' + dd;
+
+        // alert(today);
+
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          plugins: [ 'interaction', 'dayGrid' ],
+          defaultDate: today,
+          editable: true,
+          eventLimit: true, // allow "more" link when too many events
+          events: [
+            {
+              title: 'All Day Event',
+              start: '2019-08-01'
+            },
+            {
+              title: 'Long Event',
+              start: '2019-08-07',
+              end: '2019-08-10'
+            },
+            {
+              groupId: 999,
+              title: 'Repeating Event',
+              start: '2019-08-09T16:00:00'
+            },
+            {
+              groupId: 999,
+              title: 'Repeating Event',
+              start: '2019-08-16T16:00:00'
+            },
+            {
+              title: 'Conference',
+              start: '2019-08-11',
+              end: '2019-08-13'
+            },
+            {
+              title: 'Meeting',
+              start: '2019-08-12T10:30:00',
+              end: '2019-08-12T12:30:00'
+            },
+            {
+              title: 'Lunch',
+              start: '2019-08-12T12:00:00'
+            },
+            {
+              title: 'Meeting',
+              start: '2019-08-12T14:30:00'
+            },
+            {
+              title: 'Happy Hour',
+              start: '2019-08-12T17:30:00'
+            },
+            {
+              title: 'Dinner',
+              start: '2019-08-12T20:00:00'
+            },
+            {
+              title: 'Birthday Party',
+              start: '2019-08-13T07:00:00'
+            },
+            {
+              title: 'Click for Google',
+              url: 'http://google.com/',
+              start: '2019-08-28'
+            }
+          ]
+        });
+
+        calendar.render();
+      });
+
+    </script>
 
 </head>
 
@@ -170,8 +257,19 @@
                 
             </div>
         </div>
-      </aside>
-      <aside>
+    </aside>
+    <aside>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-12">
+            
+          </div>
+        </div>
+        <h2>Calendar Schedules</h2>
+        <div id='calendar'></div>
+      </div>
+    </aside>
+    <aside>
         <div class="container text-center">
             <div class="">
                 <h2>Previous Activities</h2>
